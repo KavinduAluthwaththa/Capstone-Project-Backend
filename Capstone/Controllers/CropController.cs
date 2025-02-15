a4a582b41561391931ded002960dabacfab86dfd
KavinduAluthwaththa <kabaluthwaththa@std.foc.sab.ac.lk>
2025-02-15 08:04:35 +0530

    controllers

---
+++ Capstone/Controllers/CropController.cs
@@ -0,0 +1,30 @@
+﻿using Capstone.Persistence.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CropController : ControllerBase
+    {
+        //get all crop details
+        [HttpGet]
+        public async Task<IActionResult> GetAllCrops(AppDbContext DbContext)
+        {
+            var crops = await DbContext.Crop
+                .Select(c => new SelectListItem
+                {
+                    Value = c.CropID.ToString(),
+                    Text = c.CropName
+                })
+                .ToListAsync();
+
+            return Ok(crops);
+
+        }
+
+    }
+}
